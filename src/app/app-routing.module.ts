41cebb38a87a1cd4b1712b47ba5e90b37fb1203f
milonroy26 <101338962+milonroy26@users.noreply.github.com>
2023-01-16 14:04:50 +0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { HomeComponent } from './components/home/home.component';
+import { TemplatesComponent } from './components/templates/templates.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "templates", component: TemplatesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplatesComponent } from './components/templates/templates.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplatesComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
